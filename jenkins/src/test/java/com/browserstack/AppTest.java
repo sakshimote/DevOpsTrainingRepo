ab73fc8df963cfca624ee203756afb9e8f3b2d6f
sakshimote <sakshimote19@gmail.com>
2023-04-06 21:29:53 +0530

    final commit added apptest

---
+++ jenkins/src/test/java/com/browserstack/AppTest.java
@@ -0,0 +1,21 @@
+package com.browserstack;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithTrueget()
+    {
+        assertTrue( true );
+    }
+
+}
