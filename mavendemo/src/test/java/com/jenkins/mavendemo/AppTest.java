0813c3b4e9ddd8e4550794ea8d7ce2c29c6979f8
sakshimote <sakshimote19@gmail.com>
2023-04-08 12:20:04 +0530

    new project

---
+++ mavendemo/src/test/java/com/jenkins/mavendemo/AppTest.java
@@ -0,0 +1,32 @@
+package com.jenkins.mavendemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithTrueget()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithTruefinal()
+    {
+        assertTrue( true );
+    }
+}
