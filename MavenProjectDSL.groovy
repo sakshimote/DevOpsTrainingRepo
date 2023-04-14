40ae7fd917a7b4b605783a13452731f043ac01b4
sakshimote <sakshimote19@gmail.com>
2023-04-14 11:39:06 +0530

    update

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -1,7 +1,7 @@
 job('First_Maven_Project_via_DSL'){
     description("first maven project generated on ${new Date()}, for testing dsl")
     scm{
-        git("https://github.com/sakshimote/DevOpsTrainingRepo.git",main)
+        git("https://github.com/sakshimote/DevOpsTrainingRepo.git", 'main')
     }
     triggers{
         scm('* * * * *')
